64d68ea5a8baf5f1448ab29c0c35c730db9d2f86
Charles Yang <charles.y.yang@oracle.com>
2016-01-18 19:01:37 -0500

    Add pretender

--- addon/authenticators/userapp.js
+++ addon/authenticators/userapp.js
@@ -3,18 +3,29 @@ import BaseAuthenticator from 'ember-simple-auth/authenticators/base';
 
 import UserApp from 'userapp';
 
+const {
+  Logger: { log, error, info, warn },
+  RSVP: { Promise }
+} = Ember;
+
 export default BaseAuthenticator.extend({
   /**
    * Authenticate the session.
    *
    * @method  authenticate
-   * @param   {String}              login       The login name could be either email or username
+   * @param   {String}              login       Email or Username depending on user's setting
    * @param   {String}              password    The user's password
    * @return  {Ember.RSVP.Promise}              A promise that when it resolves results in the session becoming authenticated
    * @public
    */
   authenticate(login, password) {
-
+    return new Promise((resolve, reject) => {
+      // Run it once on the backburner
+      UserApp.User.login({ login, password }, function() {
+        info(arguments);
+        resolve(true);
+      });
+    });
   },
 
   invalidate() {
--- tests/unit/authenticators/userapp-test.js
+++ tests/unit/authenticators/userapp-test.js
@@ -1,37 +1,64 @@
+import Ember from 'ember';
 import { test, moduleFor } from 'ember-qunit';
 
 import Pretender from 'pretender';
 
+
+const {
+  Logger: { log, error, info, warn },
+  typeOf
+} = Ember;
+
 moduleFor('authenticator:userapp', {
   integration: false,
 //  needs: ['authenticator:not-existing-authenticator'],
-//  setup() {
-////    this.register('authenticator:userapp', )
-//    globalVariable = 'helloworld';
-//    var internalVariable = 'helloworld';
-//  },
+  beforeEach() {
+//    this.register('authenticator:userapp', )
+    // Initialize app
+    UserApp.initialize({ appId: 'test_app_id'});
+    // Create pretender server and return true for preflight
+    this.pretenderServer = new Pretender();
+    this.pretenderServer.register('OPTIONS', '/v1/*', function() {
+      return [ 200, { "Content-Type": "application/json" }, "" ];
+    });
+  },
+
+  afterEach() {
+    this.pretenderServer.shutdown();
+  },
 });
 
-test('it exists', function(assert) {
+test('authenticator it exists', function(assert) {
   let userappAuthenticator = this.subject();
   assert.ok(!!userappAuthenticator);
 });
 
+// test('authenticator authenticate should return a promise', function(assert) {
+//   assert.ok(typeOf(this.subject().authenticate().then) === 'function');
+// });
+
 test('authenticator should fail on email not verified', function(assert) {
-  let server = new Pretender(function() {
-    this.post('/user.login', function(request) {
-      let error = {
-        "token":"6Tla5m0AS0S-7__o8WkPqw",
-        "user_id":"fL2HGWUMTbac5mPN40kq5g",
+  let server = this.pretenderServer;
+  server.post('/v1/user.login', function(request) {
+    return [
+      200,
+      { "Content-Type": "application/json" },
+      JSON.stringify({
+        "token":"random_token",
+        "user_id":"random_user_id",
         "lock_type":"EMAIL_NOT_VERIFIED",
         "locks":["EMAIL_NOT_VERIFIED"],
-      };
-
-      return [
-        200,
-        { "Content-Type": "application/json" },
-        JSON.stringify(error)
-      ];
-    });
+      })
+    ];
   });
+
+  var resolve = assert.async();
+  assert.ok(
+    this.subject()
+    .authenticate('test_username', 'test_password')
+    .then((response) => {
+      resolve();
+      info('Authenticate resolved');
+    })
+  );
 });
