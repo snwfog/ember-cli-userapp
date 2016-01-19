5c9fb33cf92f4f98fae7c8fa4bca75903cb19c26
Charles Yang <donchoa@gmail.com>
2016-01-18 22:29:44 -0500

    Add more unit test

--- addon/authenticators/userapp.js
+++ addon/authenticators/userapp.js
@@ -4,9 +4,9 @@ import BaseAuthenticator from 'ember-simple-auth/authenticators/base';
 import UserApp from 'userapp';
 
 const {
-  Logger: { log, error, info, warn },
-  RSVP: { Promise }
-} = Ember;
+        Logger: { log, error, info, warn },
+        RSVP: { Promise }
+        } = Ember;
 
 export default BaseAuthenticator.extend({
   /**
@@ -19,11 +19,47 @@ export default BaseAuthenticator.extend({
    * @public
    */
   authenticate(login, password) {
+    let _this = this;
     return new Promise((resolve, reject) => {
       // Run it once on the backburner
-      UserApp.User.login({ login, password }, function() {
-        info(arguments);
-        resolve(true);
+      UserApp.User.login({ login, password }, (error, result) => {
+        if (error) {
+          reject(error);
+        } else {
+          if (result.locks && result.locks.indexOf('EMAIL_NOT_VERIFIED') > -1) {
+            UserApp.setToken(null);
+            reject({
+              name:    'EMAIL_NOT_VERIFIED',
+              message: 'Please verify your email address by clicking on the link in the verification email that we\'ve sent you.'
+            });
+          } else if (result.locks && result.locks.length > 0) {
+            UserApp.setToken(null);
+            reject({ name: 'LOCKED', message: 'Your account has been locked.' });
+          } else {
+            if (!result.token) {
+              reject({ name: 'CANNOT_FIND_TOKEN', message: 'Cannot find a valid token.'});
+            }
+
+            UserApp.setToken(result.token);
+            _this
+              ._load()
+              .then((user) => {
+                user.token = result.token;
+                resolve(user);
+              })
+              .catch((error) => {
+                reject(error);
+              });
+          }
+        }
+      });
+    });
+  },
+
+  _load() {
+    return new Promise((resolve, reject) => {
+      UserApp.User.get({ user_id: 'self' }, (error, users) => {
+        return !!(error) ? reject(error) : resolve(users[0]);
       });
     });
   },
--- tests/unit/authenticators/userapp-test.js
+++ tests/unit/authenticators/userapp-test.js
@@ -2,10 +2,11 @@ import Ember from 'ember';
 import { test, moduleFor } from 'ember-qunit';
 
 import Pretender from 'pretender';
-
+import UserApp from 'userapp';
 
 const {
   Logger: { log, error, info, warn },
+  K,
   typeOf
 } = Ember;
 
@@ -15,11 +16,11 @@ moduleFor('authenticator:userapp', {
   beforeEach() {
 //    this.register('authenticator:userapp', )
     // Initialize app
-    UserApp.initialize({ appId: 'test_app_id'});
+    UserApp.initialize({ appId: 'test_app_id' });
     // Create pretender server and return true for preflight
     this.pretenderServer = new Pretender();
-    this.pretenderServer.register('OPTIONS', '/v1/*', function() {
-      return [ 200, { "Content-Type": "application/json" }, "" ];
+    this.pretenderServer.register('OPTIONS', 'https://api.userapp.io/v1/*', function() {
+      return [200, { "Content-Type": "application/json" }, ""];
     });
   },
 
@@ -33,32 +34,122 @@ test('authenticator it exists', function(assert) {
   assert.ok(!!userappAuthenticator);
 });
 
-// test('authenticator authenticate should return a promise', function(assert) {
-//   assert.ok(typeOf(this.subject().authenticate().then) === 'function');
-// });
-
-test('authenticator should fail on email not verified', function(assert) {
+test('authenticator authenticate should return a promise', function(assert) {
   let server = this.pretenderServer;
-  server.post('/v1/user.login', function(request) {
+  server.post('https://api.userapp.io/v1/user.login', function(request) {
+    return [
+      200,
+      { "Content-Type": "application/json" },
+      JSON.stringify({}),
+    ];
+  });
+
+  assert.ok(typeOf(this.subject().authenticate().then) === 'function');
+});
+
+test('authenticator should fail on user email not verified', function(assert) {
+  let server = this.pretenderServer;
+  server.post('https://api.userapp.io/v1/user.login', function(request) {
     return [
       200,
       { "Content-Type": "application/json" },
       JSON.stringify({
-        "token":"random_token",
-        "user_id":"random_user_id",
-        "lock_type":"EMAIL_NOT_VERIFIED",
-        "locks":["EMAIL_NOT_VERIFIED"],
+        "token":     "random_token",
+        "user_id":   "random_user_id",
+        "lock_type": "EMAIL_NOT_VERIFIED",
+        "locks":     ["EMAIL_NOT_VERIFIED"],
       })
     ];
   });
 
-  var resolve = assert.async();
-  assert.ok(
-    this.subject()
+  var resolved = assert.async();
+  var error;
+  this.subject()
     .authenticate('test_username', 'test_password')
-    .then((response) => {
-      resolve();
-      info('Authenticate resolved');
+    .catch(({ name, message }) => {
+      error = { name, message };
     })
-  );
+    .finally(() => {
+      assert.ok(error.name, 'EMAIL_NOT_VERIFIED');
+      resolved();
+    });
+});
+
+test('authenticator should fail on user lock', function(assert) {
+  let server = this.pretenderServer;
+  server.post('https://api.userapp.io/v1/user.login', function(request) {
+    return [
+      200,
+      { "Content-Type": "application/json" },
+      JSON.stringify({
+        "token":     "random_token",
+        "user_id":   "random_user_id",
+        "lock_type": "USER_ISSUED",
+        "locks":     ["USER_ISSUED"],
+      })
+    ];
+  });
+
+  var resolved = assert.async();
+  var error;
+  this.subject()
+    .authenticate('test_username', 'test_password')
+    .catch(({ name, message }) => {
+      error = { name, message };
+    })
+    .finally(() => {
+      assert.ok(error.name, 'USER_ISSUED');
+      resolved();
+    });
+});
+
+test('authenticator should return promise on success login', function(assert) {
+  let server = this.pretenderServer;
+  server.post('https://api.userapp.io/v1/user.login', function(request) {
+    return [
+      200,
+      { "Content-Type": "application/json" },
+      JSON.stringify({
+        "token":     "random_token",
+        "user_id":   "random_user_id",
+        "lock_type": "",
+        "locks":     [],
+      })
+    ];
+  });
+
+  server.post('https://api.userapp.io/v1/user.get', function(request) {
+    return [
+      200,
+      { "Content-Type": "application/json" },
+      JSON.stringify([{
+        "user_id":        "random_user_id",
+        "first_name":     "Test_User_First_Name",
+        "last_name":      "Test_User_Last_Name",
+        "email":          "test@userapp.io",
+        "email_verified": false,
+        "login":          "test@userapp.io",
+        "properties":     {},
+        "features":       {},
+        "permissions":    {},
+        "subscription":   null,
+        "lock":           null,
+        "locks":          [],
+        "ip_address":     null,
+        "last_login_at":  1453173581,
+        "updated_at":     1453173581,
+        "created_at":     1453135148
+      }])
+    ];
+  });
+
+  var resolved = assert.async();
+  this.subject()
+    .authenticate('test_username', 'test_password')
+    .then((user) => {
+      assert.ok(user['user_id'], 'random_user_id');
+    })
+    .finally(() => {
+      resolved();
+    });
 });
