1bfe8e335d0d876fd135fb4702a61d5d2ceb8944
Charles Yang <donchoa@gmail.com>
2016-04-12 19:48:19 -0400

    Reformat

--- addon/authenticators/userapp.js
+++ addon/authenticators/userapp.js
@@ -4,14 +4,14 @@ import BaseAuthenticator from 'ember-simple-auth/authenticators/base';
 import UserApp from 'userapp';
 
 const {
-  Logger: { log, error, info, warn },
-  RSVP: { Promise },
-  run
-} = Ember;
+        Logger: { log, error, info, warn },
+        RSVP: { Promise },
+        run
+        } = Ember;
 
 export default BaseAuthenticator.extend({
   _currentUser: null,
-  _token: null,
+  _token:       null,
 
   /**
    * Authenticate the session.
@@ -33,21 +33,29 @@ export default BaseAuthenticator.extend({
           if (result.locks && result.locks.indexOf('EMAIL_NOT_VERIFIED') > -1) {
             UserApp.setToken(null);
             run(null, reject, {
-              name:    'EMAIL_NOT_VERIFIED',
+              name: 'EMAIL_NOT_VERIFIED',
               message: 'Please verify your email address by clicking on the link in the verification email that we\'ve sent you.'
             });
           } else if (result.locks && result.locks.length > 0) {
             UserApp.setToken(null);
-            run(null, reject, { name: 'LOCKED', message: 'Your account has been locked.' });
+            run(null, reject, {
+              name:    'LOCKED',
+              message: 'Your account has been locked.'
+            });
           } else if (!result.token) {
-            run(null, reject, { name: 'CANNOT_FIND_TOKEN', message: 'Cannot find a valid token.'});
+            run(null, reject, {
+              name:    'CANNOT_FIND_TOKEN',
+              message: 'Cannot find a valid token.'
+            });
           } else {
             UserApp.setToken(result.token);
             _this._load().then((user) => {
-              user.token = result.token;
-              run(null, resolve, {user});
-            })
-            .catch((error) => { run(null, reject, error); });
+                user.token = result.token;
+                run(null, resolve, { user });
+              })
+              .catch((error) => {
+                run(null, reject, error);
+              });
           }
         }
       });
@@ -60,7 +68,7 @@ export default BaseAuthenticator.extend({
         if (error) {
           run(null, reject, error);
         } else {
-          run(null, resolve, users[0]);
+          run(null, resolve, users[ 0 ]);
         }
       });
     });
@@ -84,24 +92,24 @@ export default BaseAuthenticator.extend({
    */
   restore(userHash) {
     var _this = this;
-    return new Promise(function(resolve, reject) {
+    return new Promise(function (resolve, reject) {
       if (Ember.isEmpty(userHash.user) || Ember.isEmpty(userHash.user.token)) {
         run(null, reject, 'User token id was not present.');
       }
 
       UserApp.Transport.Current.call({ token: userHash.user.token }, 1, 'token.heartbeat', {},
-        function(error, result) {
+        function (error, result) {
           if (error) {
             run(null, reject, error);
           } else {
             UserApp.setToken(userHash.user.token);
-            _this._load().then(function(user) {
-              user.token = userHash.user.token;
-              run(null, resolve, { user });
-            })
-            .catch(function(error) {
-              run(null, reject, error);
-            });
+            _this._load().then(function (user) {
+                user.token = userHash.user.token;
+                run(null, resolve, { user });
+              })
+              .catch(function (error) {
+                run(null, reject, error);
+              });
           }
         });
     });
